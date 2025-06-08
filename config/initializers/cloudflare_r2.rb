a583df404d00b2ec9e1ecbeba1d82242bfab9cf0
root <root@Aegis.localdomain>
2025-06-09 01:26:27 +0900

    fix:環境変数周辺修正

--- config/initializers/cloudflare_r2.rb
+++ config/initializers/cloudflare_r2.rb
@@ -2,11 +2,7 @@ puts "[CloudflareR2Service] Initializer running..."
 
 require Rails.root.join("app/services/active_storage/service/cloudflare_r2_service")
 
-ActiveStorage::Service.url_resolver { |name, config, *_args|
-  if name == "CloudflareR2"
-    puts "[CloudflareR2Service] Configuring CloudflareR2Service..."
-    ActiveStorage::Service::CloudflareR2Service.new(**config.symbolize_keys)
-  else
-    nil # fallback to default
-  end
+ActiveStorage::Service.configure :CloudflareR2, ->(config, _options = {}) {
+  puts "[CloudflareR2Service] Configuring CloudflareR2Service..."
+  ActiveStorage::Service::CloudflareR2Service.new(**config.symbolize_keys)
 }
