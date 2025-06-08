c77ef370619dbd940c9f7519441b0517a0e142f8
root <root@Aegis.localdomain>
2025-06-08 17:59:20 +0900

    fix:アバターが破損ファイルになるのを防止

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,17 @@ module ApplicationHelper
   def image_attachment?(attachment)
     attachment.connect_type.start_with?('image')
   end
+
+  def avatar_image_tag(user, size: "50x50")
+    if user.avatar.attached?
+      url = if Rails.env.production?
+              "https://780948cb2236cfafee48ef8bc1fab47c.r2.cloudflarestorage.com/#{user.avatar.key}"
+            else
+              url_for(user.avatar)
+            end
+      image_tag(url, size: size)
+    else
+      image_tag('/images/default_avatar.png', size: size)
+    end
+  end
 end
