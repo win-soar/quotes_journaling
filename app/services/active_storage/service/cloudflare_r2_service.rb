a63eaee6af260026405fb10b0c7ed1a2a4335b9c
root <root@Aegis.localdomain>
2025-06-08 20:25:12 +0900

    fix:環境変数周辺修正

---
+++ app/services/active_storage/service/cloudflare_r2_service.rb
@@ -0,0 +1,9 @@
+class ActiveStorage::Service::CloudflareR2Service < ActiveStorage::Service::S3Service
+  private
+
+  def upload_with_checksum(key, io, checksum: nil, **options)
+    instrument :upload, key: key, checksum: checksum do
+      object_for(key).put(body: io, **options)
+    end
+  end
+end
