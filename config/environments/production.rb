8b911c587d586c0d09a3008e874c0cb22684f0ee
root <root@Aegis.localdomain>
2025-06-20 01:34:52 +0900

    fix:アバターが破損ファイルになる問題の修正

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,7 +1,7 @@
 require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
-  # config.hosts << 'www.quotes-journaling.com'
+  Rails.application.routes.default_url_options[:host] = 'https://quotes-journaling.com'
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
