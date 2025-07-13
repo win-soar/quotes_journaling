99217e8125e5abf35792e0bcfd3e6b1f176aaaca
root <root@Aegis.localdomain>
2025-07-14 07:27:26 +0900

    fix:オートコンプリート機能の修正

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -1,10 +1,8 @@
 import { Application } from "@hotwired/stimulus"
-import "controllers"
 
 const application = Application.start()
-
-// Configure Stimulus development experience
 application.debug = false
-window.Stimulus   = application
+window.Stimulus = application
 
 export { application }
+
