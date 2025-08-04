776f2e13c62767917871387b1a4809af7496bce7
root <root@Aegis.localdomain>
2025-08-05 00:31:36 +0900

    fix:google認証のバグ修正

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -4,11 +4,13 @@ module Users
 
     def google_oauth2
       @user = User.from_omniauth(request.env["omniauth.auth"])
-
+      Rails.logger.info "Omniauth user: #{@user.inspect}"
+      Rails.logger.info "Errors: #{@user.errors.full_messages}" if @user.errors.any?
       if @user.persisted?
         sign_in_and_redirect @user, event: :authentication
       else
-        redirect_to root_path
+        Rails.logger.error "Failed to persist user: #{@user.errors.full_messages}"
+        redirect_to root_path, alert: "認証に失敗しました"
       end
     end
   end
