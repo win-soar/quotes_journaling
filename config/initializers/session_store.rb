a928bc46614540afc54e2903b0dcf579e32e7151
root <root@Aegis.localdomain>
2025-08-04 23:39:40 +0900

    fix:google認証のバグ修正

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,1 +1,4 @@
-Rails.application.config.session_store :cookie_store, key: '_quotes_journaling_session'
+Rails.application.config.session_store :cookie_store,
+  key: '_quotes_journaling_session',
+  same_site: :lax,
+  secure: Rails.env.production?
