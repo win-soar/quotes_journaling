59b1764c62316decb59ef18bda15cc0b4adce2bf
root <root@Aegis.localdomain>
2025-08-09 01:04:10 +0900

    fix:管理画面からユーザー削除が出来ない不具合の修正

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_08_06_150638) do
+ActiveRecord::Schema[7.1].define(version: 2025_08_08_160101) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -135,6 +135,6 @@ ActiveRecord::Schema[7.1].define(version: 2025_08_06_150638) do
   add_foreign_key "comments", "users"
   add_foreign_key "likes", "quotes"
   add_foreign_key "likes", "users"
-  add_foreign_key "quotes", "users"
+  add_foreign_key "quotes", "users", on_delete: :cascade
   add_foreign_key "reports", "users"
 end
