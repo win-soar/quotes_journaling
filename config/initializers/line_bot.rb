95b0067da5f4a4ebc72f98df7314b0160f88a491
root <root@Aegis.localdomain>
2025-08-22 23:05:52 +0900

    fix:本日のおすすめクォーツ機能の実装

--- config/initializers/line_bot.rb
+++ config/initializers/line_bot.rb
@@ -1,14 +1,10 @@
-$line_bot_client = nil
-
-Rails.application.reloader.to_prepare do
-  require 'line/bot'
-
-  $line_bot_client = Line::Bot::Client.new do |config|
-    config.channel_secret = ENV['LINE_CHANNEL_SECRET']
-    config.channel_token = ENV['LINE_CHANNEL_TOKEN']
-  end
-end
-
 def line_client
-  $line_bot_client
+  @line_client ||= begin
+    require 'line/bot'
+
+    Line::Bot::Client.new do |config|
+      config.channel_secret = ENV['LINE_CHANNEL_SECRET']
+      config.channel_token = ENV['LINE_CHANNEL_TOKEN']
+    end
+  end
 end
