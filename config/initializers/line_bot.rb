a1f8341609f0e0f53b53c6f290787c6f6a110d3e
root <root@Aegis.localdomain>
2025-08-23 01:20:35 +0900

    fix:本日のおすすめクォーツ機能の実装

--- config/initializers/line_bot.rb
+++ config/initializers/line_bot.rb
@@ -1,33 +1,26 @@
-require 'line/bot'
+Rails.logger.info '[LINE Bot] Initializing LINE Bot client...'
 
-# LINE Botクライアントを初期化するモジュール
-module LineBot
-  class Client
-    class << self
-      def client
-        @client ||= begin
-          Rails.logger.info '[LINE Bot] Initializing LINE Bot client...'
-
-          unless ENV['LINE_CHANNEL_SECRET'].present? && ENV['LINE_CHANNEL_TOKEN'].present?
-            Rails.logger.error '[LINE Bot] エラー: 必要な環境変数が設定されていません。LINE_CHANNEL_SECRET と LINE_CHANNEL_TOKEN を確認してください。'
-            raise 'Missing LINE channel credentials'
-          end
-
-          Line::Bot::Client.new do |config|
-            config.channel_secret = ENV['LINE_CHANNEL_SECRET']
-            config.channel_token = ENV['LINE_CHANNEL_TOKEN']
-          end
-        rescue => e
-          Rails.logger.error "[LINE Bot] LINE Bot client の初期化に失敗しました: #{e.message}"
-          Rails.logger.error e.backtrace.join("\n")
-          raise
-        end
-      end
-    end
+begin
+  unless ENV['LINE_CHANNEL_SECRET'].present? && ENV['LINE_CHANNEL_TOKEN'].present?
+    Rails.logger.error '[LINE Bot] エラー: 必要な環境変数が設定されていません。LINE_CHANNEL_SECRET と LINE_CHANNEL_TOKEN を確認してください。'
+    raise 'Missing LINE channel credentials'
   end
+
+  LINE_CLIENT = Line::Bot::Client.new do |config|
+    config.channel_secret = ENV['LINE_CHANNEL_SECRET']
+    config.channel_token = ENV['LINE_CHANNEL_TOKEN']
+  end
+
+  Rails.logger.info '[LINE Bot] LINE Bot client の初期化に成功しました'
+rescue => e
+  Rails.logger.error "[LINE Bot] LINE Bot client の初期化に失敗しました: #{e.message}"
+  Rails.logger.error e.backtrace.join("\n")
+  raise
 end
 
-# グローバルに利用可能なヘルパーメソッド
 def line_client
-  LineBot::Client.client
+  LINE_CLIENT
+rescue => e
+  Rails.logger.error "[LINE Bot] LINE Bot client の取得に失敗しました: #{e.message}"
+  raise
 end
