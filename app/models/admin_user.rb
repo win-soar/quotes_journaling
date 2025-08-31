3c80bf4c6e37e53f5bc28a8a429e99871d3b8aaf
root <root@Aegis.localdomain>
2025-09-01 02:10:31 +0900

    add:テストメッセージ送信機能

--- app/models/admin_user.rb
+++ app/models/admin_user.rb
@@ -3,4 +3,9 @@ class AdminUser < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, 
          :recoverable, :rememberable, :validatable
+
+  # ActiveAdmin + Ransack 4.x対応: 検索許可属性を明示
+  def self.ransackable_attributes(auth_object = nil)
+    %w[id email created_at updated_at remember_created_at reset_password_sent_at]
+  end
 end
