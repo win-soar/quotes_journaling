a75ddd3b041056eb92a0db68b317160e152755a0
root <root@Aegis.localdomain>
2025-09-02 00:23:12 +0900

    add:テストメッセージ送信機能

--- app/services/line_client_service.rb
+++ app/services/line_client_service.rb
@@ -1,10 +1,11 @@
+require 'logger'
 require 'line/bot'
 
 class LineClientService
   def self.messaging_api_client
     @messaging_api_client ||= Line::Bot::Client.new do |config|
       config.channel_secret = ENV['LINE_CHANNEL_SECRET']
-      config.channel_token  = ENV['LINE_CHANNEL_TOKEN']
+      config.channel_token = ENV['LINE_CHANNEL_TOKEN']
     end
   end
 end
