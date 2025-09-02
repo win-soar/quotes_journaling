2ff423673fc5972809083f552694137406c892cb
root <root@Aegis.localdomain>
2025-09-03 01:00:37 +0900

    add:テストメッセージ送信機能

--- config/application.rb
+++ config/application.rb
@@ -1,7 +1,7 @@
 require_relative "boot"
 
 require "rails"
-require 'line/bot'
+require 'line/bot' unless defined?(Line::Bot)
 # Pick the frameworks you want:
 require "active_model/railtie"
 require "active_job/railtie"
