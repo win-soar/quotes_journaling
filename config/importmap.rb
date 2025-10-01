356a9bb73f2bfdb1c98f340efb203ebab7de274e
root <root@Aegis.localdomain>
2025-10-01 15:34:17 +0900

    add:CSSの追加5

---
+++ config/importmap.rb
@@ -0,0 +1,11 @@
+# Pin npm packages by running ./bin/importmap
+
+pin "application"
+pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
+pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
+pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
+pin_all_from "app/javascript/controllers", under: "controllers"
+pin "bootstrap" # @5.3.8
+pin "@popperjs/core", to: "@popperjs--core.js" # @2.11.8
+pin "@activeadmin/activeadmin", to: "active_admin.js"
+
