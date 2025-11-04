8d2807028af32eff4ed0b30dde71f1c6a7a6c8e5
root <root@Aegis.localdomain>
2025-11-04 20:44:45 +0900

    fix:RSpecエラーの解消

--- spec/mailers/previews/reset_mailer_preview.rb
+++
@@ -1,5 +0,0 @@
-# Preview all emails at http://localhost:3000/rails/mailers/reset_mailer_mailer
-class ResetMailerPreview < ActionMailer::Preview
-  # Preview this email at http://localhost:3000/rails/mailers/reset_mailer_mailer/send_when_passreset
-  delegate :send_when_passreset, to: :ResetMailer
-end
